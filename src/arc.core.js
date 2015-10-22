945615a749dfbec97577366c75c8ead27e8e0c0a
VISHAL <vishal.sharma009@yahoo.com>
2015-10-22 11:47:08 +0530

    time

--- src/arc.core.js
+++ src/arc.core.js
@@ -22,7 +22,7 @@
         
         //form builder
         formbuild: function(method,action,args){
-            var form_div = document.querySelector(this.selector);
+            form_div = document.querySelector(this.selector);
             var form_var = document.createElement('form');
             form_div.appendChild(form_var);
             form_var.setAttribute("method",method);
@@ -30,7 +30,6 @@
             
             var len = args.length;
             var params = ["type","value"];
-            
             for(i=0;i<len;i++){
                 var x = form_var.appendChild(document.createElement(args[i].split(':')[0]));
                 var y = document.createElement("br");
@@ -43,7 +42,57 @@
             
             return this;
         }
-    
     }
+        //updating time and conversion
+        
+    
+    
     arc.arcpro.initialize.prototype = arc.arcpro;
 })();
+
+
+        function dispdate()
+        {
+            //var form_div = document.querySelector(this.selector);
+            //alert("hello");
+            div1=document.createElement("div");
+            disp_time=document.createElement("p");
+            form_div.appendChild(div1);
+            div1.appendChild(disp_time);
+            
+            
+            //alert("a");
+            function clock(){
+            var time=new Date();
+            hr=time.getHours();
+            min=time.getMinutes();
+            sec=time.getSeconds();
+            if(sec<10)
+                sec="0"+sec;
+            if (min < 10)
+                min = "0" + min;
+            if(hr<10)
+                hr="0"+hr;
+            if(hr>12)
+                { tod="PM";
+                  hr=hr-12;
+                }
+            else
+                tod="AM";
+                nowtime=hr+":"+min+":"+sec+ " "+" "+tod;
+                
+                
+                //disp_time.removeChild();
+                var t=setTimeout(function(){clock()},1000);
+            }
+                clock();
+            now=document.createTextNode(nowtime);
+                disp_time.appendChild(now);
+            
+            
+            
+        
+            //alert(nowtime);
+            
+            
+        }
